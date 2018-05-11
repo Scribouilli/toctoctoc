97578cdbc602d7060dea0c77b117d420aa62d722
pntbr <stephane.langlois@scopyleft.fr>
2018-05-11 13:49:16 +0200

    Add login page

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-var http = require("http");
-var server = http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write("<!DOCTYPE \"html\">");
-  response.write("<html>");
-  response.write("<head>");
-  response.write("<title>Hello World Page</title>");
-  response.write("</head>");
-  response.write("<body>");
-  response.write("Hello World!");
-  response.write("</body>");
-  response.write("</html>");
-  response.end();
-});
+const http = require('http')
+const server = http.createServer(function(request, response) {
+  response.writeHead(200, {'Content-Type': 'text/html'})
+  response.end(`<!doctype html>
+  <html lang=en>
+      <head>
+          <meta charset=utf-8>
+          <meta name="referrer" content="same-origin">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          
+          <title> </title>
+          
+          <meta name="description" content=" ">
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          
+          <link rel="stylesheet" href="https://rawgit.com/twbs/bootstrap/v4-dev/dist/css/bootstrap-reboot.css">
+          
+          <script crossorigin="anonymous" src="https://cdn.polyfill.io/v2/polyfill.min.js"></script>
+      </head>
+      <body>
+        <a href=https://github.com/login/oauth/authorize?client_id=2b4ed9ba835b05f83e2d>Login with Github</button>
+      </body>
+  </html>
+  `)
+})
 
-server.listen(process.env.PORT || 5000);
-console.log("Server is listening");
+server.listen(process.env.PORT || 5000)
+console.log("Server is listening")
