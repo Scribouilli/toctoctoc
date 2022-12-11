da69e7726d30c089d14157709dc0023dc64356fb
David Bruant <davidbruant@protonmail.com>
2022-12-11 17:53:49 +0100

    Rajout d'erreurs en cas de paramètres manquants sur /gh-callback + passage à l'API WHATWG URL

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 //@ts-check
 
-import { resolve } from 'path'
-import { parse, format } from 'url'
-import { readFileSync } from 'fs'
+import { resolve } from 'node:path'
+import { readFileSync } from 'node:fs'
 
 import Fastify from 'fastify'
 import got from 'got'
@@ -15,23 +14,35 @@ const client_secret = process.env.GITHUB_SECRET
 const whitelist = new Set(readFileSync('./whitelist.csv', {encoding: 'utf8'}).split('\n').map(s => s.trim()))
 
 server.get('/gh-callback', (req, res) => {
-  const {code, destination='/receive-token'} = req.query
+  const {code, destination} = req.query
 
+  if(!code){
+    res.status(400)
+      .send(`<h1>Erreur 400</h1><p>le paramètre <code>code<code> est manquant.</p>`)
+    return;
+  }
+  if(!destination){
+    res.status(400)
+      .send(`<h1>Erreur 400</h1><p>le paramètre <code>destination<code> est manquant.</p>`)
+    return;
+  }
+
+  const redirectUrl = new URL(destination)
+  const hostname = redirectUrl.hostname 
+
+  if(!hostname || whitelist.has(hostname)){
+    res.status(403)
+      .send(`<h1>Erreur 403</h1><p>Vous avez demandé : ${destination}, et ${hostname} n'est pas présent dans notre <a href="https://github.com/daktary-team/file-moi-les-clefs/blob/master/whitelist.csv">liste d'invité</a>.</p>`)
+    return;
+  }
+  
   const urlGhOAuth =
     `https://github.com/login/oauth/access_token?code=${code}&client_id=${client_id}&client_secret=${client_secret}`
 
   got.post(urlGhOAuth, { json: true }).then(ghResponse => {
     const access_token = ghResponse.body.access_token
-    const redirectUrl = parse(destination, true)
-    const hostname = redirectUrl.hostname // hostname is null when destination is a relative url
-
-    redirectUrl.query.access_token = access_token
-
-    if (!hostname || whitelist.has(hostname)) {
-      res.redirect(302, format(redirectUrl))
-    } else {
-      res.send(403, `<h1>403 Error</h1><p>Vous avez demandé : ${destination}, et ${hostname} n'est pas présent dans notre <a href="https://github.com/daktary-team/file-moi-les-clefs/blob/master/whitelist.csv">liste d'invité</a>.</p>`)
-    }
+    redirectUrl.searchParams.set(`access_token`, access_token)
+    res.redirect(302, redirectUrl.toString())
   })
 })
 
@@ -63,6 +74,7 @@ server.get('/\*' , (req, res) => {
   `)
 })
 
+// @ts-ignore
 server.listen({ port: process.env.PORT || 5000 }, (err, address) => {
     console.log(`Server is listening on ${address}  `)
 }) 
