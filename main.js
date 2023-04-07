959c030a5515297b62013eb2ba1b350649f4cebd
David Bruant <davidbruant@protonmail.com>
2023-04-07 15:19:17 +0200

    Suppression du fichier et d'un endpoint inutiles + rajout d'en template HTML minimal partout

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 //@ts-check
 
-import { resolve } from 'node:path'
-import { readFileSync, createReadStream } from 'node:fs'
+import { readFileSync } from 'node:fs'
 
 import Fastify from 'fastify'
 import got from 'got'
@@ -19,7 +18,6 @@ if(!process.env.PORT){
   process.exit(1);
 }
 
-
 const client_id = process.env.GITHUB_OAUTH_APP_CLIENT_ID
 const client_secret = process.env.GITHUB_OAUTH_APP_CLIENT_SECRET
 const port = process.env.PORT
@@ -28,6 +26,34 @@ const host = process.env.HOST || 'localhost'
 const allowlist = new Set(
   readFileSync('./allowlist.csv', {encoding: 'utf8'}).split('\n').map(s => s.trim())
 )
+
+/**
+ * 
+ * @param {TemplateStringsArray | string} content
+ * @returns {string}
+ */
+function htmlTemplate(content){
+  if(typeof content !== 'string'){
+    content = content[0]
+  }
+
+  return `<!doctype html>
+    <html lang="fr">
+        <head>
+            <meta charset="utf-8">
+            <link rel="icon" href="data:,">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            
+            <title>Serveur toctoctoc</title>
+            
+            <meta name="referrer" content="no-referrer">
+            <style>body{width: 60%; margin: 1rem auto;}</style>
+        </head>
+        <body>
+          ${content}
+        </body>
+    </html>`
+}
 
 const server = Fastify()
 
@@ -38,7 +64,7 @@ server.get('/github-callback', (req, res) => {
   if(!code){
     res.status(400)
       .header('Content-Type', 'text/html')
-      .send(`
+      .send(htmlTemplate`
         <h1>Erreur</h1>
         <p>le paramètre <code>code<code> est manquant</p>
         <p>Peut-être que l'API github ne fonctionne plus pareil. Regarder l'API Rest github</p>
@@ -48,7 +74,7 @@ server.get('/github-callback', (req, res) => {
   if(!destination){
     res.status(400)
       .header('Content-Type', 'text/html')
-      .send(`
+      .send(htmlTemplate`
         <h1>Erreur</h1>
         <p>le paramètre <code>destination<code> est manquant.</p>
         <p>Il est sûrement manquant en tant que paramètre du <code>redirect_uri</code> du lien "login with github"
@@ -62,7 +88,7 @@ server.get('/github-callback', (req, res) => {
   if(!hostname){
     res.status(400)
       .header('Content-Type', 'text/html')
-      .send(`
+      .send(htmlTemplate`
         <h1>Erreur</h1>
         <p>le paramètre <code>destination<code> n'a pas de hostname. (destination : ${destination})</p>
         <p>Rajouter une origine au paramètre <code>destination<code>
@@ -73,16 +99,16 @@ server.get('/github-callback', (req, res) => {
   if(hostname !== 'localhost' && !allowlist.has(hostname)){
     res.status(403)
       .header('Content-Type', 'text/html')
-      .send(`
+      .send(htmlTemplate(`
         <h1>Erreur</h1>
         <p>La destination est ${destination}, et son hostname (${hostname}) n'est pas présent dans notre <a href="https://github.com/Scribouilli/toctoctoc/blob/main/allowlist.csv">liste de hostname autorisés</a>.</p>
         <p>Liste des hostname autorisés : 
           <ul>
-            ${[...allowlist].map(hostname => `<li>${hostname}</li>`)}
+            ${[...allowlist].map(hostname => `<li>${hostname}</li>`).join('')}
           </ul>
         </p>
         <p>Changer cette liste ou installez une nouvelle instance de toctoctoc où ce hostname est autorisé</p>
-      `)
+      `))
     return;
   }
 
@@ -95,7 +121,7 @@ server.get('/github-callback', (req, res) => {
     if(!access_token){
       res.status(400)
         .header('Content-Type', 'text/html')
-        .send(`
+        .send(htmlTemplate`
           <h1>Erreur</h1>
           <p>le <code>access_token</code> attendu de la part de Github n'a pas été récupéré</p>
           <p>Peut-être que le fonctionnement de l'API github a changé ou que le code ne le trouve pas au bon endroit</p>
@@ -108,35 +134,21 @@ server.get('/github-callback', (req, res) => {
   })
 })
 
-server.get('/receive-token', (req, res) => {
-  res.header('Content-Type', 'application/octet-stream')
-  res.send(createReadStream(resolve('./example_access_token.html')))
-})
 
-server.get('/\*' , (req, res) => {
+server.get('/' , (req, res) => {
 
   res.header('Content-Type', 'text/html')
-  res.send(`<!doctype html>
-    <html lang=en>
-        <head>
-            <meta charset=utf-8>
-            <meta name="referrer" content="same-origin">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            
-            <title></title>
-            
-            <meta name="description" content=" ">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            
-            <link rel="stylesheet" href="https://rawgit.com/twbs/bootstrap/v4-dev/dist/css/bootstrap-reboot.css">
-            
-            <script crossorigin="anonymous" src="https://cdn.polyfill.io/v2/polyfill.min.js"></script>
-        </head>
-        <body>
-          <a href="https://github.com/login/oauth/authorize?client_id=${client_id}&scope=public_repo">Login with Github</button>
-        </body>
-    </html>
-  `)
+  res.send(htmlTemplate(`
+    <h1>Serveur toctoctoc<h1>
+    <p>Le serveur toctoctoc est disponible (<code>client_id: ${client_id}</code>)</p>
+    <p>Tu peux créer un bouton "login with github" où le <code>redirect_uri</code> contient un 
+      paramètre <code>destination</code> vers l'un des domaines suivants :
+      <ul>
+        ${[...allowlist].map(hostname => `<li>${hostname}</li>`).join('')}
+      </ul>
+    </p>
+
+  `))
 })
 
 // @ts-ignore
