8598a480fa4458bad84f4d622ecab5354b8d1cbf
Fanny Cheung <dev@ynote.hk>
2023-10-10 11:29:05 +0200

    Extract Github callback handler

---
+++ github/index.js
@@ -0,0 +1,78 @@
+import got from 'got'
+
+export const onGithubCallback = (client_id, client_secret) => (req, res) => {
+  // @ts-ignore
+  const {code, destination} = req.query
+
+  if(!code){
+    res.status(400)
+      .header('Content-Type', 'text/html')
+      .send(htmlTemplate`
+        <h1>Erreur</h1>
+        <p>le paramètre <code>code<code> est manquant</p>
+        <p>Peut-être que l'API github ne fonctionne plus pareil. Regarder l'API Rest github</p>
+      `)
+    return;
+  }
+  if(!destination){
+    res.status(400)
+      .header('Content-Type', 'text/html')
+      .send(htmlTemplate`
+        <h1>Erreur</h1>
+        <p>le paramètre <code>destination<code> est manquant.</p>
+        <p>Il est sûrement manquant en tant que paramètre du <code>redirect_uri</code> du lien "login with github"
+      `)
+    return;
+  }
+
+  const redirectUrl = new URL(destination)
+  const hostname = redirectUrl.hostname
+
+  if(!hostname){
+    res.status(400)
+      .header('Content-Type', 'text/html')
+      .send(htmlTemplate`
+        <h1>Erreur</h1>
+        <p>le paramètre <code>destination<code> n'a pas de hostname. (destination : ${destination})</p>
+        <p>Rajouter une origine au paramètre <code>destination<code>
+      `)
+    return;
+  }
+
+  if(hostname !== 'localhost' && !allowlist.has(hostname)){
+    res.status(403)
+      .header('Content-Type', 'text/html')
+      .send(htmlTemplate(`
+        <h1>Erreur</h1>
+        <p>La destination est ${destination}, et son hostname (${hostname}) n'est pas présent dans notre <a href="https://github.com/Scribouilli/toctoctoc/blob/main/allowlist.csv">liste de hostname autorisés</a>.</p>
+        <p>Liste des hostname autorisés :
+          <ul>
+            ${[...allowlist].map(hostname => `<li>${hostname}</li>`).join('')}
+          </ul>
+        </p>
+        <p>Changer cette liste ou installez une nouvelle instance de toctoctoc où ce hostname est autorisé</p>
+      `))
+    return;
+  }
+
+  const urlGithubOAuth =
+    `https://github.com/login/oauth/access_token?code=${code}&client_id=${client_id}&client_secret=${client_secret}`
+
+  got.post(urlGithubOAuth, { json: true }).then(githubResponse => {
+    const access_token = new URLSearchParams(githubResponse.body).get('access_token')
+
+    if(!access_token){
+      res.status(400)
+        .header('Content-Type', 'text/html')
+        .send(htmlTemplate`
+          <h1>Erreur</h1>
+          <p>le <code>access_token</code> attendu de la part de Github n'a pas été récupéré</p>
+          <p>Peut-être que le fonctionnement de l'API github a changé ou que le code ne le trouve pas au bon endroit</p>
+        `)
+      return;
+    }
+
+    redirectUrl.searchParams.set(`access_token`, access_token)
+    res.redirect(302, redirectUrl.toString())
+  })
+}
--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 //@ts-check
 
+import { onGithubCallback } from './github/index.js'
+
 import { readFileSync } from 'node:fs'
 
 import Fastify from 'fastify'
-import got from 'got'
 
 if(!process.env.GITHUB_OAUTH_APP_CLIENT_ID){
   console.error(`Il manque la variable d'environnement "GITHUB_OAUTH_APP_CLIENT_ID"`)
@@ -57,91 +58,15 @@ function htmlTemplate(content){
 
 const server = Fastify()
 
-server.get('/github-callback', (req, res) => {
-  // @ts-ignore
-  const {code, destination} = req.query
-
-  if(!code){
-    res.status(400)
-      .header('Content-Type', 'text/html')
-      .send(htmlTemplate`
-        <h1>Erreur</h1>
-        <p>le paramètre <code>code<code> est manquant</p>
-        <p>Peut-être que l'API github ne fonctionne plus pareil. Regarder l'API Rest github</p>
-      `)
-    return;
-  }
-  if(!destination){
-    res.status(400)
-      .header('Content-Type', 'text/html')
-      .send(htmlTemplate`
-        <h1>Erreur</h1>
-        <p>le paramètre <code>destination<code> est manquant.</p>
-        <p>Il est sûrement manquant en tant que paramètre du <code>redirect_uri</code> du lien "login with github"
-      `)
-    return;
-  }
-
-  const redirectUrl = new URL(destination)
-  const hostname = redirectUrl.hostname
-
-  if(!hostname){
-    res.status(400)
-      .header('Content-Type', 'text/html')
-      .send(htmlTemplate`
-        <h1>Erreur</h1>
-        <p>le paramètre <code>destination<code> n'a pas de hostname. (destination : ${destination})</p>
-        <p>Rajouter une origine au paramètre <code>destination<code>
-      `)
-    return;
-  }
-
-  if(hostname !== 'localhost' && !allowlist.has(hostname)){
-    res.status(403)
-      .header('Content-Type', 'text/html')
-      .send(htmlTemplate(`
-        <h1>Erreur</h1>
-        <p>La destination est ${destination}, et son hostname (${hostname}) n'est pas présent dans notre <a href="https://github.com/Scribouilli/toctoctoc/blob/main/allowlist.csv">liste de hostname autorisés</a>.</p>
-        <p>Liste des hostname autorisés : 
-          <ul>
-            ${[...allowlist].map(hostname => `<li>${hostname}</li>`).join('')}
-          </ul>
-        </p>
-        <p>Changer cette liste ou installez une nouvelle instance de toctoctoc où ce hostname est autorisé</p>
-      `))
-    return;
-  }
-
-  const urlGithubOAuth =
-    `https://github.com/login/oauth/access_token?code=${code}&client_id=${client_id}&client_secret=${client_secret}`
-
-  got.post(urlGithubOAuth, { json: true }).then(githubResponse => {
-    const access_token = new URLSearchParams(githubResponse.body).get('access_token')
-
-    if(!access_token){
-      res.status(400)
-        .header('Content-Type', 'text/html')
-        .send(htmlTemplate`
-          <h1>Erreur</h1>
-          <p>le <code>access_token</code> attendu de la part de Github n'a pas été récupéré</p>
-          <p>Peut-être que le fonctionnement de l'API github a changé ou que le code ne le trouve pas au bon endroit</p>
-        `)
-      return;
-    }
-
-    redirectUrl.searchParams.set(`access_token`, access_token)
-    res.redirect(302, redirectUrl.toString())
-  })
-})
+server.get('/github-callback', onGithubCallback(client_id, client_secret))
 
 
 server.get('/' , (req, res) => {
-
   res.header('Content-Type', 'text/html')
   res.send(htmlTemplate(`
     <h1>Serveur toctoctoc</h1>
     <p>Le serveur toctoctoc est disponible (<code>client_id: ${client_id}</code>)</p>
-    <p>Tu peux créer un bouton "login with github" où le <code>redirect_uri</code> contient un 
+    <p>Tu peux créer un bouton "login with github" où le <code>redirect_uri</code> contient un
       paramètre <code>destination</code> vers l'un des domaines suivants :
       <ul>
         ${[...allowlist].map(hostname => `<li>${hostname}</li>`).join('')}
